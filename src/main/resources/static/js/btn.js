a984f3638e4f0d1dc26ee89ff74ba94690d08a9b
Matias <maticampopiano@hotmail.com>
2023-06-30 17:59:11 -0300

    codigo de mas pero funciona, prueba modales

--- src/main/resources/static/js/btn.js
+++ src/main/resources/static/js/btn.js
@@ -1,31 +1,40 @@
 document.getElementById('eliminarBtn').addEventListener('click', function() {
-    // Obtener los checkboxes seleccionados
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    var ids = [];
-    for (var i = 0; i < checkboxes.length; i++) {
-        var id = checkboxes[i].id.replace('checkbox_', '');
-        ids.push(id);
-    }
-    
-    // Enviar los IDs al servidor para eliminar los registros
-    
-    // Ejemplo de código para enviar una solicitud AJAX utilizando fetch
-    fetch('/eliminarAlumnos', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(ids)
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Eliminar las filas correspondientes a los registros eliminados
-        data.forEach(id => {
-            var row = document.getElementById(`checkbox_${id}`).closest('tr');
-            row.parentNode.removeChild(row);
+    // Abrir la ventana modal antes de obtener los checkboxes seleccionados
+    $('#confirmModal').modal('show');
+
+    // Evento de confirmación en la ventana modal
+    document.getElementById('confirmarEliminarBtn').addEventListener('click', function() {
+        // Obtener los checkboxes seleccionados
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        var ids = [];
+        for (var i = 0; i < checkboxes.length; i++) {
+            var id = checkboxes[i].id.replace('checkbox_', '');
+            ids.push(id);
+        }
+
+        // Enviar los IDs al servidor para eliminar los registros
+
+        // Ejemplo de código para enviar una solicitud AJAX utilizando fetch
+        fetch('/eliminarAlumnos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(ids)
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Eliminar las filas correspondientes a los registros eliminados
+            data.forEach(id => {
+                var row = document.getElementById(`checkbox_${id}`).closest('tr');
+                row.parentNode.removeChild(row);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error:', error);
+        
+        // Cerrar la ventana modal después de confirmar la eliminación
+        $('#confirmModal').modal('hide');
     });
 });
